1f28b02a0469add2ec1358cdef7125f1aefcaaa9
Vishwas Mukund <vishwas.mukund@gmail.com>
2020-05-16 15:11:27 -0400

    convert to programmatic views

--- client/Editable Profile/Editable Profile/ProfileController.swift
+++ client/Editable Profile/Editable Profile/ProfileController.swift
@@ -8,11 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ProfileController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .red
     }
 
 
